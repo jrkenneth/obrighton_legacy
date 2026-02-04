3e79be3e9ba98e1c9797e4b52e131fbb4cb90ec1
Kenneth.igharo <igharoken@gmail.com>
2026-02-04 08:12:47 +0400

    Fix: Comment out alternative database connection options in dbconnect_sample.php

--- _include/dbconnect_sample.php
+++ _include/dbconnect_sample.php
@@ -12,4 +12,10 @@ if ($host != "localhost:8888") {
 }else{
     $con = mysqli_connect("localhost","uname","pword","dbase") or die ("Connection was not Established");
 }
+
+// if ($host != "localhost:8888") {
+//     $con = mysqli_connect("localhost","tryntmta_obuser","W__R-iut$@Ac&","tryntmta_obportal") or die ("Connection was not Established");
+// }else{
+//     $con = mysqli_connect("localhost","root","root","obrighton") or die ("Connection was not Established");
+// }
 ?>
